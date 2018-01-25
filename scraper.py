e5e4a17d7ee26bf6be6a8703130eb4d2b41a4445
LilaRa <35800324+LilaRa@users.noreply.github.com>
2018-01-25 14:54:43 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,12 +12,11 @@ record={}
 root = lxml.html.fromstring(html)
 names = root.cssselect("td div a")
 for name in names:
-  print name.text
+  #print name.text
   print name.attrib ['href']
   record ['link']=name.attrib['href']
   print record
-    
-#
+  scraperwiki.sqlite.save(unique_keys=['link'], data=record)  
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
